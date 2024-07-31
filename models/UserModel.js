c272c21f938e4bbe0bd25a5f1e5b6c031cfbc2a0
antonio0304 <anthonyganchozo10@hotmail.com>
2024-07-30 23:07:39 -0500

    solcuion_correo

---
+++ models/UserModel.js
@@ -0,0 +1,32 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../db/conexion.js';
+
+const UserModel = sequelize.define('UserModel', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  state: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+  },
+}, {
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default UserModel;
